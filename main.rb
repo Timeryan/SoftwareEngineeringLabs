cc462fc17237784992398d92bb79ef07d07cba25
Timeryan <timeryan2001@gmail.com>
2021-03-22 18:25:28 +0300

    Вторая лабораторная
    task1: Написать скрипт, который будет принимать число и слово, если слово заканчивается на “CS” - выводит на экран цифру 2 в степени (длины введенного слова), если не заканчивается - выводит слово задом наперед
    
    task2:  Написать скрипт, который будет выводить массив покемонов
    Спросит сколько добавить покемонов
    Указанное на предыдущем этапе число раз спросит имя и цвет каждого покемона
    Выведет в консоль массив содержащий хеши покемонов в формате
    [{ name: 'Pikachu', color: 'Yellow' }]равно 20 - возвращает его, в противном случае выводит сумму этих чисел.

--- main.rb
+++ main.rb
@@ -1,45 +1,39 @@
 
-def task_1()
-puts 'Введите имя:'
-user_fname = gets.chomp
-puts 'Введите фамилию:'
-user_lname = gets.chomp
-puts 'Введите возвраст:'
-user_age = gets.to_i
-
-if user_age < 18
-    puts "Привет #{user_fname} #{user_lname}, тебе меньше 18 лет, но начать учиться програмированию никогда не рано"
-else
-    puts "Привет #{user_fname} #{user_lname} самое время заняться делом"
-end
+def task_1
+    puts("Введите число и слово >>")
+    text = gets.chomp
+    ar = text.split(" ")
+    if ar[1].end_with?("CS")
+        puts(ar[0].to_i**ar[1].length)
+    else
+        puts(ar[1].reverse)
+    end
 end
 
-def task_2()
-
-puts 'Введите первое число:'
-a = gets.to_i
-
-puts 'Введите второе число:'
-b = gets.to_i
-
-if a ==20
-    puts "#{a}"
-elsif b == 20
-    puts "#{b}" 
-else
-    puts a+b
+def task_2
+    puts("Cколько покемонов добавить =>")
+    count = gets.to_i
+    pokemons = []
+    count.times do |i|
+        puts("Укажите имя #{i+1} покемона =>")
+        name = gets.chomp
+        puts("Укажите цвет #{i+1} покемона =>")
+        color = gets.chomp
+        pokemons<<{"name": name, "color":color}
+    end
+    puts(pokemons)
 end
-end
+
+
 
 def task_3
-    check = 1
     loop do
         print "0 - выйти из программы \n1 - первое задание \n2 - второе задание \n>> "
     check = gets.to_i
     if check == 1
         task_1
-        elsif check == 2
-            task_2
+    elsif check == 2
+        task_2
     elsif check == 0 
     break
     end 
