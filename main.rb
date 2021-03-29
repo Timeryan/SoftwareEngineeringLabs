109f121054a1f51d8095c64789c03d1635925169
Timeryan <timeryan2001@gmail.com>
2021-03-29 13:25:09 +0300

    Третья лабораторная
    task1: У вас есть файл, со следующей структурой и содержащий не менее 10 строк: Имя, фамилия  и возраст
           Прим.
           Иван Иванов 20
           Петр Петров 21
     Необходимо прочитать файл, затем запросив у пользователя ввод возраста, найти в результатах чтения файла, студента чей возраст равен введенному числу и записать этого студента(тов) в другой файл с названием results.txt. После этого перезапросить ввод.
           Программа завершается выводом на экран содержимого файла results.txt построчно, если все студенты из первого файла были записаны во второй или если пользователь ввел с клавиатуры -1
    
    task2: Напишите программу, которая начинается с чтения банковского баланса клиента из файла с именем balance.txt. Этот файл содержит одну строку со стартовым балансом клиента. Если файл не существует, используйте стартовый баланс 100.0, который должен быть константой.
           После этого программа повторно предложит клиенту внести деньги, вывести деньги, проверить баланс или выйти, используя буквы D (deposit), W (withdraw), B (balance) и Q (quit). Программа должна принимать на вход значения в верхнем или нижнем регистре.
           Для депозитов (D), программа подсказывает сумму. Сумма должна быть больше нуля. Если сумма действительна, программа добавляет сумму депозита к балансу клиента и отображает новый баланс.
           При снятии средств (W) программа выдаст запрос на сумму. Сумма должна быть больше нуля и меньше или равна текущему балансу. Если сумма корректна, то программа вычитает сумму вывода из баланса клиента и отображает новый баланс.
           Для проверки баланса (B) программа просто выводит текущий баланс.
    Когда клиент решит выйти из программы (Q), программа запишет текущий баланс обратно в файл balance.txt.
    В случае неправильного ввода (команда или сумма), ваша программа должна выдать соответствующее сообщение об ошибке, которое говорит клиенту, как ее исправить. Нельзя просто выводить "Error!" - это не поможет.

--- main.rb
+++ main.rb
@@ -1,42 +1,94 @@
-
 def task_1
-    puts("Введите число и слово >>")
-    text = gets.chomp
-    ar = text.split(" ")
-    if ar[1].end_with?("CS")
-        puts(ar[0].to_i**ar[1].length)
-    else
-        puts(ar[1].reverse)
+  file_data = File.read("input.txt").split ("\n")
+  loop do
+    puts("Введите возраст =>")
+    age = gets.chomp
+    if age == "-1"
+      break
     end
+    file_data.length.times do |i|
+      if file_data[i].end_with?(age)
+        File.write("result.txt", "#{file_data[i]}\n", mode: "a")
+      end
+    end
+    result_data = File.read("result.txt").split("\n")
+    file_data.delete_if { |res| result_data.include? res }
+    if file_data.length == 0
+      break
+    end
+  end
+  file_output_data = File.read("result.txt")
+  puts(file_output_data)
+end
+
+def deposit(balance, add_sum)
+  if add_sum > 0
+    puts("Вы внесли на счёт: #{add_sum}$")
+    balance += add_sum
+    puts("Ваш баланс: #{balance}$")
+    balance
+  elsif
+  puts "Сумма не валидна(меньше нуля)"
+  end
+end
+def withdraw(balance, get_sum)
+  if get_sum < 0
+    puts "Сумма не валидна(меньше нуля)"
+  elsif balance < get_sum
+    puts "У вас на счету не сдостаточно средст"
+  else
+    puts("Вы сняли со счёта: #{get_sum}$")
+    balance -= get_sum
+    puts("Ваш баланс: #{balance}$")
+    balance
+  end
+end
+
+def get_balance(balance)
+    puts("Ваш баланс: #{balance}$")
 end
 
 def task_2
-    puts("Cколько покемонов добавить =>")
-    count = gets.to_i
-    pokemons = []
-    count.times do |i|
-        puts("Укажите имя #{i+1} покемона =>")
-        name = gets.chomp
-        puts("Укажите цвет #{i+1} покемона =>")
-        color = gets.chomp
-        pokemons<<{"name": name, "color":color}
+
+  balance = File.file?("balance.txt") ? File.read("balance.txt") : 100.0
+  balance = balance.to_f
+
+  loop do
+    puts "D - внести деньги\nW - вывести деньги\nB - проверить баланс\nQ - выйти"
+    x = gets.chomp
+    x = x.upcase
+    case x
+    when "D"
+      puts "Введите сумму для депозита >>"
+      add_sum = gets.to_f
+      balance = deposit(balance,add_sum)
+    when "W"
+      puts "Введите сумму для снятия со счета >>"
+      get_sum = gets.to_f
+      balance = withdraw(balance,get_sum)
+    when "B"
+      get_balance(balance)
+    when "Q"
+      File.write("balance.txt", balance)
+      return
+    else
+      puts "Неизвестная команда"
     end
-    puts(pokemons)
+  end
 end
-
-
 
 def task_3
-    loop do
-        print "0 - выйти из программы \n1 - первое задание \n2 - второе задание \n>> "
+  loop do
+    print "0 - выйти из программы \n1 - первое задание \n2 - второе задание \n>> "
     check = gets.to_i
     if check == 1
-        task_1
+      task_1
     elsif check == 2
-        task_2
-    elsif check == 0 
-    break
-    end 
+      task_2
+    elsif check == 0
+      break
+    end
+  end
 end
-end
+
 task_3
